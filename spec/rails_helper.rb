0980f1faab917fa74f5228a6efc89246b6348366
boni <boni@twine.hu>
2017-01-27 11:27:39 +0100

    adding coverage to tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start do
+  load_profile 'rails'
+  add_group 'Api', 'app/api'
+  minimum_coverage 95
+  minimum_coverage_by_file 90
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
