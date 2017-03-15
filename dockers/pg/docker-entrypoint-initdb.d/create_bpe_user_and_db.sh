f86f476102af55fd954e51a5b1db110fc0e56ea2
boni <boni@twine.hu>
2017-03-15 02:58:24 +0100

    docker compose seems working binding.pry works with docker attach and can be detached ctrl+p ctrl+q

--- dockers/pg/docker-entrypoint-initdb.d/create_bpe_user_and_db.sh
+++ dockers/pg/docker-entrypoint-initdb.d/create_bpe_user_and_db.sh
@@ -4,6 +4,7 @@ set -e
 DB_NAME="$APP_NAME_$RAILS_ENV"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER bpe;
-    CREATE DATABASE bpe_test;
-    GRANT ALL PRIVILEGES ON DATABASE bpe_test TO bpe;
+    ALTER USER bpe WITH PASSWORD 'password';
+    CREATE DATABASE bpe_development;
+    GRANT ALL PRIVILEGES ON DATABASE bpe_development TO bpe;
 EOSQL
